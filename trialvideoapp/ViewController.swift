90a3910fc4281bf2b2b0bd1927fb5b8a7d10db53
Mustafa Boorenie <mustafa.boorenie@gmail.com>
2020-11-28 14:02:58 +0000

    initial commit

---
+++ trialvideoapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trialvideoapp
+//
+//  Created by Mustafa Boorenie on 28/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
